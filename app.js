b87c3206e940ab579ea0393f9117fa2850edf9d0
Daniel Stroebel <dan.stroebel@gmail.com>
2018-05-24 13:32:11 +0200

    added details page

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/yelp_camp");
 
-var campgrounds = [
-    {name:"Fish Creek", image: "https://muenchen-res.cloudinary.com/.imaging/stk/responsive/image980/dms/lhm/tourismus/camping-l/document/camping-l.jpg"},
-    {name:"Haunte Grove", image:"https://bestbrands-4001.kxcdn.com/wp-content/uploads/2015/12/camping-tent.jpg"},
-    {name: "Ranger's Delight", image:"http://s3.amazonaws.com/ncc-ccn/images/_cardWide/camping-card.jpg?mtime=20170425165109" },
-    {name: "Reihenhaus", image:"http://s3.amazonaws.com/ncc-ccn/images/_cardWide/group-camping.jpg?mtime=20180426155434"},
-    {name:"Snowy Rivers", image:"http://s3.amazonaws.com/ncc-ccn/images/_cardWide/winter-camping.jpg?mtime=20180425155227"},
-    {name:"Autumn Splendor", image:"https://www.nps.gov/buff/planyourvisit/images/backpacking2.jpg?maxwidth=650&autorotate=false"}
-];
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create( {
+//     name:"Autumn Splendor", 
+//     image:"https://www.nps.gov/buff/planyourvisit/images/backpacking2.jpg?maxwidth=650&autorotate=false",
+//     description: "Nice for summer, too cold for late autumn."
+// });  
 
 
 app.get("/", function (req, res) {
@@ -20,21 +27,38 @@ app.get("/", function (req, res) {
   });
 
 app.get("/campground", function (req, res) {
-    res.render("campground", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    })
    });
 
 app.post("/campground", function (req, res) {
     const name = req.body.name;
     const image = req.body.image;
-    const newCampground = {name: name, image:image};
-    campgrounds.push(newCampground);
-    res.redirect("campground");
+    const description = req.body.description;
+    const newCampground = {name: name, image:image, description:description};
+    Campground.create(newCampground, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("campground");
+        }
+    });    
 });
 
 app.get("/campground/new", function (req, res) {
     res.render("new");
    });
 
+app.get("/campground/:id", function (req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground){
+        res.render("show", {campground: foundCampground});
+    })
+   });
 
 app.listen(3000, function () {
     console.log("YelpCamp ready.");
